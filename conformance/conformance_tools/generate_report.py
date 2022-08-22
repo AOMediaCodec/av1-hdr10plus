ba2a9732f7ca109e5e61139eed78eb04e5349898
Dimitri Podborski <dpodborski@apple.com>
2022-08-22 09:49:56 -0700

    dump to file and print stats

--- conformance/conformance_tools/generate_report.py
+++ conformance/conformance_tools/generate_report.py
@@ -6,28 +6,16 @@ import os
 import re
 import sys
 
-from .utils import execute_cmd, read_json
+from .utils import dump_to_json, execute_cmd, read_json
 
 ASSERT_PATTERN = r"assert-.{8}"
 
 
-def main():
-    """Entry point"""
-
-    ret = execute_cmd('bikeshed spec ../index.bs temp.html')
-
-    if ret.returncode != 0:
-        print('Could not execute bikeshed')
-        sys.exit(-1)
-    with open('temp.html', 'r', encoding='utf-8') as file:
-        html = file.read()
-    asserts = re.findall(ASSERT_PATTERN, html)
-
-    print('index.html asserts:')
-    conf_file_asserts = {}
-    for asrt in asserts:
-        print(asrt)
-        conf_file_asserts[asrt] = {'sucess': [], 'warn': [], 'err': []}
+def collect_file_asserts(assert_ids, spec):
+    """Given an array of assert IDs return all asserts from conformance files"""
+    all_asserts = {}
+    for assert_id in assert_ids:
+        all_asserts[assert_id] = {'ok': [], 'warn': [], 'err': []}
 
     # conformance file asserts
     for root, _subdirs, files in os.walk('conformance_files'):
@@ -36,5 +24,76 @@ def main():
             if input_extension not in ['.json']:
                 continue
             input_path = os.path.join(root, conf_file)
+
             json = read_json(input_path)
-            print(json['validation']['warnings'])
+            for spec_name in json['validation']:
+                if spec_name['spec'] != spec:
+                    continue
+                # ok
+                for check in spec_name['successful_checks']:
+                    if check['rule_id'] not in all_asserts:
+                        print(
+                            f"Error: unknown assert {check['rule_id']} in {input_path}")
+                        sys.exit(1)
+                    all_asserts[check['rule_id']
+                                ]['ok'].append(input_path)
+                # warn
+                for check in spec_name['warnings']:
+                    if check['rule_id'] not in all_asserts:
+                        print(
+                            f"Error: unknown assert {check['rule_id']} in {input_path}")
+                        sys.exit(1)
+                    all_asserts[check['rule_id']
+                                ]['warn'].append(input_path)
+                # err
+                for check in spec_name['errors']:
+                    if check['rule_id'] not in all_asserts:
+                        print(
+                            f"Error: unknown assert {check['rule_id']} in {input_path}")
+                        sys.exit(1)
+                    all_asserts[check['rule_id']
+                                ]['err'].append(input_path)
+    return all_asserts
+
+
+def print_coverage_stats(all_asserts):
+    """show how many assets are covered by conformance files"""
+    cnt = len(all_asserts)
+    coverage = []
+    print('Coverage statistics')
+    print('='*36)
+    print(f'{"":15}{"":>7}{"invalid  ":>14}')
+    print(f'{"assert_id":15}{"OK":>7}{"warn":>7}{"err":>7}')
+    print('-'*36)
+    for assert_id in all_asserts:
+        cnt_ok = len(all_asserts[assert_id]['ok'])
+        cnt_warn = len(all_asserts[assert_id]['warn'])
+        cnt_err = len(all_asserts[assert_id]['err'])
+        print(f'{assert_id}{cnt_ok:7}{cnt_warn:7}{cnt_err:7}')
+        coverage.append([assert_id, cnt_ok,  cnt_warn, cnt_err])
+    print('='*36)
+
+    valid = [x for x in coverage if x[1] > 0]
+    invalid = [x for x in coverage if x[2] > 0 or x[3] > 0]
+
+    cov_valid = 100.0 * len(valid) / cnt
+    cov_invalid = 100.0 * len(invalid) / cnt
+
+    print(f'Valid coverage:   {cov_valid:6.2f}% ({len(valid)} from {cnt})')
+    print(f'Invalid coverage: {cov_invalid:6.2f}% ({len(invalid)} from {cnt})')
+
+
+def main():
+    """Entry point"""
+    ret = execute_cmd('bikeshed spec ../index.bs temp.html')
+
+    if ret.returncode != 0:
+        print('Could not execute bikeshed')
+        sys.exit(-1)
+    with open('temp.html', 'r', encoding='utf-8') as file:
+        html = file.read()
+    assert_ids = re.findall(ASSERT_PATTERN, html)
+
+    all_asserts = collect_file_asserts(assert_ids, 'av1hdr10plus')
+    dump_to_json('assert-map.json', all_asserts)
+    print_coverage_stats(all_asserts)
