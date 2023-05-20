1402044480a282faf71398eac14d1b67a747f440
Dimitri Podborski <dpodborski@apple.com>
2023-05-19 18:16:30 -0700

    generate report

--- conformance/conformance_tools/generate_report.py
+++ conformance/conformance_tools/generate_report.py
@@ -3,105 +3,173 @@ Module to generate conformance report
 requires bikeshed to be installed
 """
 import os
-import re
-import sys
 
+from bs4 import BeautifulSoup
 from .utils import dump_to_json, execute_cmd, read_json
 
-ASSERT_PATTERN = r"assert-.{8}"
+
+def get_coverage_data(rules_from_spec, rules_from_files):
+    '''
+    For each spec rule add matched file entries with descriptions
+    '''
+    coverage_data = []
+
+    for rule_spec in rules_from_spec:
+        rule_coverage = {
+            'id': rule_spec['id'],
+            'description': rule_spec['description'],
+            'successful_checks': [],
+            'errors': [],
+            'warnings': []
+        }
+
+        for rule_file in rules_from_files:
+            for check_type in ['successful_checks', 'errors', 'warnings']:
+                for check in rule_file[check_type]:
+                    if check['id'] == rule_spec['id']:
+                        rule_coverage[check_type].append({
+                            'file_path': rule_file['file_path'],
+                            'description': check['description']
+                        })
+
+        coverage_data.append(rule_coverage)
+    return coverage_data
 
 
-def collect_file_asserts(assert_ids, spec):
-    """Given an array of assert IDs return all asserts from conformance files"""
-    all_asserts = {}
-    for assert_id in assert_ids:
-        all_asserts[assert_id] = {'ok': [], 'warn': [], 'err': []}
-
-    # conformance file asserts
-    for root, _subdirs, files in os.walk('conformance_files'):
-        for conformance_file in files:
-            _input_filename, input_extension = os.path.splitext(conformance_file)
-            if input_extension not in ['.json']:
-                continue
-            input_path = os.path.join(root, conformance_file)
-
-            json = read_json(input_path)
-
-            cw_report = json['compliance_warden']
-            if not cw_report:
-                continue
-
-            print('FIXME: ComplianceWarden needs to support bikeshed asserts. Continue after the feature is added.')
-            sys.exit(-1)
-
-            for spec_validation in cw_report['validation']:
-                if spec_validation['specification'] != spec:
-                    continue
-                # ok
-                for check in spec_validation['successful_checks']:
-                    if check['rule_id'] not in all_asserts:
-                        print(
-                            f"Error: unknown assert {check['rule_id']} in {input_path}")
-                        sys.exit(1)
-                    all_asserts[check['rule_id']
-                                ]['ok'].append(input_path)
-                # warn
-                for check in spec_validation['warnings']:
-                    if check['rule_id'] not in all_asserts:
-                        print(
-                            f"Error: unknown assert {check['rule_id']} in {input_path}")
-                        sys.exit(1)
-                    all_asserts[check['rule_id']
-                                ]['warn'].append(input_path)
-                # err
-                for check in spec_validation['errors']:
-                    if check['rule_id'] not in all_asserts:
-                        print(
-                            f"Error: unknown assert {check['rule_id']} in {input_path}")
-                        sys.exit(1)
-                    all_asserts[check['rule_id']
-                                ]['err'].append(input_path)
-    return all_asserts
+def filter_validation_entries(spec_validation):
+    '''
+    Make sure that each validation entry has id, details and description only
+    '''
+    for key in ['successful_checks', 'errors', 'warnings', 'unchecked']:
+        filtered_list = []
+        for item in spec_validation[key]:
+            if 'id' in item:
+                filtered_item = {
+                    'id': item.get('id', None),
+                    'details': item.get('details', None),
+                    'description': item.get('description', None)
+                }
+                filtered_list.append(filtered_item)
+        spec_validation[key] = filtered_list
+        if 'specification' in spec_validation:
+            del spec_validation['specification']
+    return spec_validation
 
 
-def print_coverage_stats(all_asserts):
-    """show how many assets are covered by conformance files"""
-    cnt = len(all_asserts)
-    coverage = []
-    print('Coverage statistics')
-    print('='*36)
-    print(f'{"":15}{"":>7}{"invalid  ":>14}')
-    print(f'{"assert_id":15}{"OK":>7}{"warn":>7}{"err":>7}')
-    print('-'*36)
-    for assert_id in all_asserts:
-        cnt_ok = len(all_asserts[assert_id]['ok'])
-        cnt_warn = len(all_asserts[assert_id]['warn'])
-        cnt_err = len(all_asserts[assert_id]['err'])
-        print(f'{assert_id}{cnt_ok:7}{cnt_warn:7}{cnt_err:7}')
-        coverage.append([assert_id, cnt_ok,  cnt_warn, cnt_err])
-    print('='*36)
+def duplicate_spec_rules_found(rules):
+    '''
+    Check if for all rules the assert-id's are unique
+    '''
+    descriptions = {}
+    duplicates_found = False
+    for rule in rules:
+        desc = rule['description']
+        if desc in descriptions:
+            descriptions[desc][0] += 1
+            duplicates_found = True
+        else:
+            descriptions[desc] = [1, rule['id']]
 
-    valid = [x for x in coverage if x[1] > 0]
-    invalid = [x for x in coverage if x[2] > 0 or x[3] > 0]
+    if duplicates_found:
+        print("ATTENTION: Duplicate rules found. See CSV below")
+        print("assert-id,description")
+        for description, info in descriptions.items():
+            if info[0] > 1:
+                print(f"{info[1]},{description.replace(',', ';')}")
 
-    cov_valid = 100.0 * len(valid) / cnt
-    cov_invalid = 100.0 * len(invalid) / cnt
-
-    print(f'Valid coverage:   {cov_valid:6.2f}% ({len(valid)} from {cnt})')
-    print(f'Invalid coverage: {cov_invalid:6.2f}% ({len(invalid)} from {cnt})')
+    return duplicates_found
 
 
-def main():
-    """Entry point"""
+def get_rules_from_spec():
+    '''
+    Compile the spec using bikeshed command and extract a list of rules
+    [{'id': '...', 'description': '...'}, ]
+    '''
     ret = execute_cmd('bikeshed spec ../index.bs temp.html')
 
     if ret.returncode != 0:
         print('Could not execute bikeshed')
-        sys.exit(-1)
-    with open('temp.html', 'r', encoding='utf-8') as file:
-        html = file.read()
-    assert_ids = re.findall(ASSERT_PATTERN, html)
+        return None
+    with open('temp.html', 'r', encoding='utf-8') as spec_file:
+        soup = BeautifulSoup(spec_file, features="lxml")
+    os.remove('temp.html')
+    assert_spans = soup.find_all('span',
+                                 {'id': lambda L: L and L.startswith('assert-')})
 
-    all_asserts = collect_file_asserts(assert_ids, 'av1hdr10plus')
-    dump_to_json('assert-map.json', all_asserts)
-    print_coverage_stats(all_asserts)
+    rules = []
+    for assert_span in assert_spans:
+        rules.append({'id': assert_span.get('id'),
+                      "description": assert_span.text.replace('"', '')})
+    return rules
+
+
+def get_rules_from_files(spec):
+    '''
+    Get rules from the file json's
+    '''
+    rules_from_files = []
+    for root, _subdirs, files in os.walk('conformance_files'):
+        for conf_file in files:
+            _input_filename, input_extension = os.path.splitext(conf_file)
+            if input_extension not in ['.json']:
+                continue
+            input_path = os.path.join(root, conf_file)
+
+            json_string = read_json(input_path)
+
+            cw_report = json_string['compliance_warden']
+            if not cw_report:
+                continue
+
+            for spec_validation in cw_report['validation']:
+                if spec_validation['specification'] != spec:
+                    continue
+                spec_validation = filter_validation_entries(spec_validation)
+                spec_validation['file_path'] = input_path
+                rules_from_files.append(spec_validation)
+    return rules_from_files
+
+
+def print_coverage_stats(coverage_data):
+    total_rules = len(coverage_data)
+    covered_rules = 0
+    uncovered_rules = []
+
+    for rule in coverage_data:
+        print(f'Rule id: {rule["id"]}')
+        print(f'Description: {rule["description"]}')
+
+        total_checks = 0
+        for check_type in ['successful_checks', 'errors', 'warnings']:
+            total_checks += len(rule[check_type])
+            if rule[check_type]:
+                print(f"{check_type}:")
+                for check in rule[check_type]:
+                    print(f'\tFile path: {check["file_path"]}')
+                    print(f'\tDescription: {check["description"]}')
+
+        if total_checks > 0:
+            covered_rules += 1
+        else:
+            uncovered_rules.append(rule['id'])
+
+        print("\n")
+
+    print(f'Overall coverage: {(covered_rules/total_rules)*100}%')
+    print(f'Uncovered rules: {uncovered_rules}')
+
+
+def main():
+    """Entry point"""
+    rules_from_spec = get_rules_from_spec()
+    # Abort if there are rules with the same id.
+    if duplicate_spec_rules_found(rules_from_spec):
+        print("Make sure the assert-id's are unique for every rule!")
+        return -1
+    # get data from file json's for this specification
+    rules_from_files = get_rules_from_files(spec='av1hdr10plus')
+    # build coverage data
+    coverage_data = get_coverage_data(rules_from_spec, rules_from_files)
+    # store and print
+    print_coverage_stats(coverage_data)
+    dump_to_json('coverage.json', coverage_data)
