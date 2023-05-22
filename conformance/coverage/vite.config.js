8a985f6f461a4368559975da537279eb25e08bd1
Deniz Ugur <d_ugur@apple.com>
2023-05-22 16:12:26 -0700

    fix coverage page path

--- conformance/coverage/vite.config.js
+++ conformance/coverage/vite.config.js
@@ -2,7 +2,7 @@ import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/pg_hdr10plus/coverage/",
+    base: "/av1-hdr10plus/coverage/",
     plugins: [preact()],
     server: {
         fs: {
