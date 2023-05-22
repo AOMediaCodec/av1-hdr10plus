1f7e8a02cea05964176ec9ea42f548085f4e901b
Dimitri Podborski <dpodborski@apple.com>
2023-05-22 11:59:45 -0700

    update (thaks Deniz)

--- conformance/aom-coverage/src/App.jsx
+++ conformance/aom-coverage/src/App.jsx
@@ -1,6 +1,8 @@
 import { useMemo } from "preact/hooks";
 import coverage_raw from "../../coverage.json";
 
+const BASE_URL = "https://aomediacodec.github.io/av1-hdr10plus/";
+
 export default function App() {
     const coverage = useMemo(() => {
         const covered = {
@@ -70,80 +72,140 @@ export default function App() {
             <h2 className="text-2xl font-bold">
                 {(coverage.covered.percentage * 100).toFixed(2)}% covered
             </h2>
-            <div className="flex flex-row items-stretch justify-between gap-6">
-                <table className="border-1">
-                    <thead>
-                        <tr>
-                            <th colSpan={2}>
-                                <h3 className="bg-black text-xl font-bold text-white">
-                                    Covered
-                                </h3>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-green-400">
-                            <td colSpan={2}>
-                                <th>Successful</th>
-                            </td>
-                        </tr>
-                        <tr className="bg-green-400">
-                            <td>
-                                <th># of files</th>
-                            </td>
-                            <td>
-                                <th>Description</th>
-                            </td>
-                        </tr>
-                        {coverage.covered.successful.map((item) => (
-                            <tr key={item.id}>
-                                <td className="w-36 text-center">
-                                    {
-                                        coverage.covered.file_counts.successful[
-                                            item.id
-                                        ]
-                                    }
-                                </td>
-                                <td>{item.description}</td>
+            <span className="border-l-4 border-red-400 pl-2 text-xl">
+                <b>WARNING: </b> These files are still <u>under review.</u>
+            </span>
+            <div className="flex flex-row items-start justify-between gap-6">
+                <div className="flex flex-[1] flex-col items-stretch justify-center gap-6">
+                    <table className="border-1">
+                        <thead>
+                            <tr>
+                                <th colSpan={3}>
+                                    <h3 className="bg-black text-xl font-bold text-white">
+                                        Covered
+                                    </h3>
+                                </th>
                             </tr>
-                        ))}
-                        <tr className="bg-red-400">
-                            <td colSpan={2}>
-                                <th>Error</th>
-                            </td>
-                        </tr>
-                        {coverage.covered.error.map((item) => (
-                            <tr key={item.id}>
-                                <td className="w-36 text-center">
-                                    {
-                                        coverage.covered.file_counts.error[
-                                            item.id
-                                        ]
-                                    }
+                        </thead>
+                        <tbody>
+                            <tr className="bg-green-400">
+                                <td colSpan={3}>
+                                    <th>Successful</th>
                                 </td>
-                                <td>{item.description}</td>
                             </tr>
-                        ))}
-                        <tr className="bg-yellow-400">
-                            <td colSpan={2}>
-                                <th>Warning</th>
-                            </td>
-                        </tr>
-                        {coverage.covered.warning.map((item) => (
-                            <tr key={item.id}>
-                                <td className="w-36 text-center">
-                                    {
-                                        coverage.covered.file_counts.warning[
-                                            item.id
-                                        ]
-                                    }
+                            <tr className="bg-green-400">
+                                <td>
+                                    <th># of files</th>
                                 </td>
-                                <td>{item.description}</td>
+                                <td>
+                                    <th>Assert ID</th>
+                                </td>
+                                <td>
+                                    <th>Description</th>
+                                </td>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <table className="border-1">
+                            {coverage.covered.successful.map((item) => (
+                                <tr key={item.id}>
+                                    <td className="text-center">
+                                        {
+                                            coverage.covered.file_counts
+                                                .successful[item.id]
+                                        }
+                                    </td>
+                                    <td className="text-center">
+                                        <a
+                                            className="text-blue-600 hover:text-blue-500"
+                                            href={`${BASE_URL}#${item.id}`}
+                                        >
+                                            {item.id}
+                                        </a>
+                                    </td>
+                                    <td className="whitespace-normal">
+                                        {item.description}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <table className="border-1">
+                        <thead>
+                            <tr>
+                                <th colSpan={3}>
+                                    <h3 className="bg-black text-xl font-bold text-white">
+                                        Covered (invalid files on purpose)
+                                    </h3>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="bg-red-400">
+                                <td colSpan={3}>
+                                    <th>Error</th>
+                                </td>
+                            </tr>
+                            <tr className="bg-red-400">
+                                <td>
+                                    <th># of files</th>
+                                </td>
+                                <td>
+                                    <th>Assert ID</th>
+                                </td>
+                                <td>
+                                    <th>Description</th>
+                                </td>
+                            </tr>
+                            {coverage.covered.error.map((item) => (
+                                <tr key={item.id}>
+                                    <td className="text-center">
+                                        {
+                                            coverage.covered.file_counts.error[
+                                                item.id
+                                            ]
+                                        }
+                                    </td>
+                                    <td className="text-center">
+                                        <a
+                                            className="text-blue-600 hover:text-blue-500"
+                                            href={`${BASE_URL}#${item.id}`}
+                                        >
+                                            {item.id}
+                                        </a>
+                                    </td>
+                                    <td className="whitespace-normal">
+                                        {item.description}
+                                    </td>
+                                </tr>
+                            ))}
+                            <tr className="bg-yellow-400">
+                                <td colSpan={3}>
+                                    <th>Warning</th>
+                                </td>
+                            </tr>
+                            {coverage.covered.warning.map((item) => (
+                                <tr key={item.id}>
+                                    <td className="text-center">
+                                        {
+                                            coverage.covered.file_counts
+                                                .warning[item.id]
+                                        }
+                                    </td>
+                                    <td className="text-center">
+                                        <a
+                                            className="text-blue-600 hover:text-blue-500"
+                                            href={`${BASE_URL}#${item.id}`}
+                                        >
+                                            {item.id}
+                                        </a>
+                                    </td>
+                                    <td className="whitespace-normal">
+                                        {item.description}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <table className="flex-[1] border-1">
                     <thead>
                         <tr>
                             <th colSpan={2}>
@@ -156,7 +218,7 @@ export default function App() {
                     <tbody>
                         <tr className="bg-neutral-400">
                             <td>
-                                <th>ID</th>
+                                <th>Assert ID</th>
                             </td>
                             <td>
                                 <th>Description</th>
@@ -164,8 +226,17 @@ export default function App() {
                         </tr>
                         {coverage.not_covered.map((item) => (
                             <tr key={item.id}>
-                                <td className="w-36 text-center">{item.id}</td>
-                                <td>{item.description}</td>
+                                <td className="text-center">
+                                    <a
+                                        className="text-blue-600 hover:text-blue-500"
+                                        href={`${BASE_URL}#${item.id}`}
+                                    >
+                                        {item.id}
+                                    </a>
+                                </td>
+                                <td className="whitespace-normal">
+                                    {item.description}
+                                </td>
                             </tr>
                         ))}
                     </tbody>
