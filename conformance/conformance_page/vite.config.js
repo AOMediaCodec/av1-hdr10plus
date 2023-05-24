9cf6a6ec668bc8d67de229a1c5248ed8ceb350a5
Dimitri Podborski <dpodborski@apple.com>
2023-05-24 11:54:10 -0700

    rename webpage folder

--- conformance/conformance_page/vite.config.js
+++ conformance/conformance_page/vite.config.js
@@ -2,7 +2,7 @@ import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/av1-hdr10plus/coverage/",
+    base: "/av1-hdr10plus/conformance/",
     plugins: [preact()],
     server: {
         fs: {
